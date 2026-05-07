394186798679a5754a6584f403878a2ebd887a60
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-05-07 18:13:01 +0100

    build(nix): widen cabal source filter to nix/ and specs/
    Include top-level Nix modules in the cabal source so haskell.nix re-evaluates when files such as nix/iog-tools.nix change. Include specs/ explicitly so future spec or documentation consumers can read from the spec-kit artifacts without another source-filter change.

--- nix/project.nix
+++ nix/project.nix
@@ -4,33 +4,45 @@
 # because Feature 001 produces node-accepted Cardano genesis and key material
 # using upstream Cardano packages as libraries.
 let
-  fix-libs = { lib, pkgs, ... }: {
-    packages.cardano-crypto-praos.components.library.pkgconfig =
-      lib.mkForce [ [ pkgs.libsodium-vrf ] ];
-    packages.cardano-crypto-class.components.library.pkgconfig =
-      lib.mkForce [[ pkgs.libsodium-vrf pkgs.secp256k1 pkgs.libblst ]];
-  };
-in pkgs.haskell-nix.cabalProject' {
+  fix-libs =
+    { lib, pkgs, ... }:
+    {
+      packages.cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf ] ];
+      packages.cardano-crypto-class.components.library.pkgconfig = lib.mkForce [
+        [
+          pkgs.libsodium-vrf
+          pkgs.secp256k1
+          pkgs.libblst
+        ]
+      ];
+    };
+in
+pkgs.haskell-nix.cabalProject' {
   name = "cardano-testnet-baker";
 
   src = pkgs.haskell-nix.haskellLib.cleanSourceWith {
     name = "cardano-testnet-baker-src";
     src = ../.;
-    filter = path: _type:
+    filter =
+      path: _type:
       builtins.match ".*\\.(cabal|hs|project|md)$" path != null
       || builtins.match ".*/src(/.*)?$" path != null
       || builtins.match ".*/app(/.*)?$" path != null
       || builtins.match ".*/test(/.*)?$" path != null
+      || builtins.match ".*/nix/[^/]+\\.nix$" path != null
       || builtins.match ".*/compose(/.*)?$" path != null
       || builtins.match ".*/examples(/.*)?$" path != null
       || builtins.match ".*/schemas(/.*)?$" path != null
+      || builtins.match ".*/specs(/.*)?$" path != null
       || builtins.match ".*/cabal\\.project$" path != null
       || builtins.match ".*/LICENSE$" path != null;
   };
 
   compiler-nix-name = "ghc9123";
 
-  inputMap = { "https://chap.intersectmbo.org/" = CHaP; };
+  inputMap = {
+    "https://chap.intersectmbo.org/" = CHaP;
+  };
 
   modules = [ fix-libs ];
 
@@ -42,6 +54,10 @@ in pkgs.haskell-nix.cabalProject' {
       hlint = { };
       haskell-language-server = { };
     };
-    buildInputs = with pkgs; [ just nixfmt-classic shellcheck ];
+    buildInputs = with pkgs; [
+      just
+      nixfmt-classic
+      shellcheck
+    ];
   };
 }
